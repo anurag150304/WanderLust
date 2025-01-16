88f994b7303701632051fe6265133d8b9de8085b
Anurag Mishra <anurag.mishra150304@gmail.com>
2025-01-17 00:07:09 +0530

    file

--- main.js
+++ main.js
@@ -17,17 +17,17 @@ const errHandler = require('./utils/errHandler');
 
 const App = express();
 const port = 8080;
-const url1 = 'mongodb://127.0.0.1:27017/wanderLust';
-// const url2 = `${process.env.ATLASDB_URL}`;
+// const url1 = 'mongodb://127.0.0.1:27017/wanderLust';
+const url2 = `${process.env.ATLASDB_URL}`;
 
 main().then(() => console.log('connection successfull')).catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect(url1);
+    await mongoose.connect(url2);
 }
 
 const store = MongoStore.create({
-    mongoUrl: url1,
+    mongoUrl: url2,
     crypto: {
         secret: `${process.env.SECRET}`
     },
